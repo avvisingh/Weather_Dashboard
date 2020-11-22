4e5e95d10e1e9edc6502bcd244e8e829049df4c2
Avvi Singh <singh.avvi98@gmail.com>
2020-11-22 17:13:31 +1100

    API request to Mapbox and OpenWeather now working

--- geocode.js
+++ geocode.js
@@ -1,24 +1,32 @@
-const submitBttn = document.getElementById('location-search-button');
-const locationInput = document.getElementById('search-location');
-
-const geocode = (address, callback) => {
+const geocode = (address) => {
     address = encodeURIComponent(locationInput.value);
 
-    const queryURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYXZ2aXNpbmdoIiwiYSI6ImNrZ2l4YjFnMzBhd3kzMnBpNnJxd25uMXgifQ.UyfTNowjHn_ZL1OuGedYug&limit=1';
+    const query1URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYXZ2aXNpbmdoIiwiYSI6ImNrZ2l4YjFnMzBhd3kzMnBpNnJxd25uMXgifQ.UyfTNowjHn_ZL1OuGedYug&limit=1';
 
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(({ features }) => {
-        var lat = features[0].center[0];
-        var long = features[0].center[1];
+        url: query1URL,
+        method: "GET",
+        success: ({ features }) => {
+            var lat = encodeURIComponent(features[0].center[0]);
+            var long = encodeURIComponent(features[0].center[1]);
 
-        console.log(lat);
-        console.log(long);
+            console.log(lat);
+            console.log(long);
+
+            const query2URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=821c074a78abf4fa2fadf03d18db1e79`;
+
+            try {
+                $.ajax({
+                    url: query2URL,
+                    method: "GET",
+                    success: (res) => {
+                        console.log(res);
+                    }
+                })
+            } catch (err) {
+                console.log(err);
+            }
+
+        }
     })
-}
-
-
-submitBttn.addEventListener('click', geocode);
-
-
+};
