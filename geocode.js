fe63110ff6e9c2656e47477b188a0528387bc7a8
Avvi Singh <singh.avvi98@gmail.com>
2020-11-27 14:31:25 +1100

    AJAX Functionality up and running with basic DOM Manipulation in-place

--- geocode.js
+++ geocode.js
@@ -1,32 +1,79 @@
 const geocode = (address) => {
     address = encodeURIComponent(locationInput.value);
 
+
     const query1URL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiYXZ2aXNpbmdoIiwiYSI6ImNrZ2l4YjFnMzBhd3kzMnBpNnJxd25uMXgifQ.UyfTNowjHn_ZL1OuGedYug&limit=1';
 
-    $.ajax({
-        url: query1URL,
-        method: "GET",
-        success: ({ features }) => {
-            var lat = encodeURIComponent(features[0].center[0]);
-            var long = encodeURIComponent(features[0].center[1]);
+    try {
+        $.ajax({
+            url: query1URL,
+            method: "GET",
+            success: ({ features }) => {
+                console.dir(features);
 
-            console.log(lat);
-            console.log(long);
+                var locationSearched = features[0].place_name;
+                var lat = encodeURIComponent(features[0].center[1]);
+                var long = encodeURIComponent(features[0].center[0]);
 
-            const query2URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=821c074a78abf4fa2fadf03d18db1e79`;
+                console.log(locationSearched);
+                console.log(lat);
+                console.log(long);
 
-            try {
-                $.ajax({
-                    url: query2URL,
-                    method: "GET",
-                    success: (res) => {
-                        console.log(res);
-                    }
-                })
-            } catch (err) {
-                console.log(err);
+                const query2URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&appid=821c074a78abf4fa2fadf03d18db1e79`;
+
+                try {
+                    $.ajax({
+                        url: query2URL,
+                        method: "GET",
+                        success: (res) => {
+                            console.dir(res);
+
+                            const unixTimeStamp = res.current.dt;
+                            const milliseconds = unixTimeStamp * 1000;
+                            const dateObject = new Date(milliseconds);
+                            const currentTemp = res.current.temp;
+                            const currentHumidity = res.current.humidity;
+                            const currentWindSpeed = res.current.wind_speed;
+                            const weatherIcon = res.current.weather[0].icon;
+                            const weatherIconSrc = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+                            const weatherDescriptionType = res.current.weather[0].description;
+                            const UVRating = res.current.uvi;
+                            console.log(typeof UVRating);
+
+                            const weekday = dateObject.toLocaleString('en-US', { weekday: 'long' });
+                            const day = dateObject.toLocaleString('en-US', { day: 'numeric' });
+                            const month = dateObject.toLocaleString('en-US', { month: 'long' });
+                            const year = dateObject.toLocaleString('en-US', { year: 'numeric' });
+                            const dateOfSearch = `Date: ${weekday}, ${day}-${month}-${year}`;
+
+                            dateDisplay.textContent = dateOfSearch;
+                            locationDisplay.textContent = locationSearched;
+                            currentTempDisplay.textContent = `Current Temperature: ${currentTemp} Degrees`;
+                            currentHumidityDisplay.textContent = `Current Humidity: ${currentHumidity}%`;
+                            currentWindSpeedDisplay.textContent = `Current Wind Speed: ${currentWindSpeed} metres/second`;
+                            weatherIconDisplay.setAttribute('src', weatherIconSrc);
+                            weatherDescription.textContent = `The current Weather Description is: "${weatherDescriptionType}"`;
+
+                            if (UVRating <= 2) {
+                                UVIDisplay.textContent = `The Midday UV Index today is ${UVRating}, which is rated as "Favourable"`;
+                                UVIDisplay.setAttribute('style', 'background-color:#40e02b;color:white');
+                            } else if (UVRating > 2 && UVRating <= 5) {
+                                UVIDisplay.textContent = `The Midday UV Index today is ${UVRating}, which is rated as "Moderate". It is not recommended to spend time under the Sun unprotected.`;
+                                UVIDisplay.setAttribute('style', 'background-color:#ebe70e;color:white');
+                            } else {
+                                UVIDisplay.textContent = `The Midday UV Index today is ${UVRating}, which is rated as "Severe". It is not recommended to spend time under the Sun unprotected.`;
+                                UVIDisplay.setAttribute('style', 'background-color:#e31809;color:white');
+                            }
+                        }
+                    })
+                } catch (err) {
+                    console.log(err);
+                }
+
             }
+        })
+    } catch (err) {
 
-        }
-    })
+    }
+
 };
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const submitBttn = document.getElementById('location-search-button');
 const locationInput = document.getElementById('search-location');
-
+const locationDisplay = document.getElementById('location-searched');
+const dateDisplay = document.getElementById('date-display');
+const currentTempDisplay = document.getElementById('current-temp');
+const currentHumidityDisplay = document.getElementById('current-humidity');
+const currentWindSpeedDisplay = document.getElementById('current-wind-speed');
+const weatherIconDisplay = document.getElementById('current-weather-icon');
+const weatherDescription = document.getElementById('weather-description');
+const UVIDisplay = document.getElementById('uv-index');
 
 submitBttn.addEventListener('click', (e) => {
     e.preventDefault();
 
     geocode(locationInput);
 
+    // localStorage.setItem()
 })
